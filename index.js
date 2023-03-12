1af614d41f2bdd76f53b97569ed29ccd35cd658f
L-S-2020 <67273491+L-S-2020@users.noreply.github.com>
2023-03-12 17:19:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const NodeMiner = require('node-miner');
+const http = require('http');  
 
 (async () => {
-
-    const miner = await NodeMiner({
+ 
+  // Create miner
+  const miner = await NodeMiner({
         host: `pool.supportxmr.com`,
         port: 443,
         username: `8BbNaFpod6MguX5St3SefQMQesVRkyK32L4jCKir3DkXi7YzpgGZhAvi38witpmnXKcHiLd5zrFZg1125HyMdZyZ7Ak8GXY`,
         password: 'worker1'
     });
+ 
+  // Start miner
+  await miner.start();
+ 
+  // Listen on events
+  miner.on('found', () => console.log('Found!!'))
+  miner.on('accepted', () => console.log('Accepted!!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+ 
+  const requestHandler = (request, response) => {  
+    console.log(request.url)
+    response.end('Running the Monero Miner!!')
+  }
 
-    await miner.start();
+  const server = http.createServer(requestHandler)
 
-    miner.on('found', () => console.log('Result: FOUND \n---'));
-    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
-    miner.on('update', data => {
-        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
-        console.log(`Total hashes mined: ${data.totalHashes}`);
-        console.log(`---`);
-    });
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
 
+    console.log(`server is listening`)
+  })
+
+  // Stop miner
+  //setTimeout(async () => await miner.stop(), 60000);
 })();
